43cd6073b308afdc2e16b595bfad2d54779c5ff2
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-09-22 15:09:14 +0300

    HOCs, withUser

--- src/HOCs/index.js
+++ src/HOCs/index.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { ThemeContext, UserContext } from '../contexts'
 
-export const withTheme = (WrapperComponent)=>{
-    return function(props){
+export const withTheme = WrapperComponent => {
+  return function (props) {
+    return (
+      <ThemeContext.Consumer>
+        {([theme, setTheme]) => (
+          <WrapperComponent theme={theme} setTheme={setTheme} {...props} />
+        )}
+      </ThemeContext.Consumer>
+    )
+  }
+}
+export const withUser = WrapperComponent => {
+  return class WrapperComponentWithUser extends Component {
+    render () {
       return (
-        <ThemeContext.Consumer>
-          {([theme, setTheme])=><WrapperComponent theme={theme} setTheme={setTheme}/>}
-        </ThemeContext.Consumer>
-    
+        <UserContext.Consumer>
+          {([user, userLogout]) => (
+            <WrapperComponent
+              user={user}
+              userLogout={userLogout}
+              {...this.props}
+            />
+          )}
+        </UserContext.Consumer>
       )
     }
   }
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,45 +3,35 @@ import { ThemeContext, UserContext } from '../../contexts'
 import style from './Header.module.css'
 import cx from 'classnames'
 import CONSTANTS from '../../constants'
-import Switch from '@mui/material/Switch';
-import { withTheme } from '../../HOCs'
+import Switch from '@mui/material/Switch'
+import { withTheme, withUser } from '../../HOCs'
 const { THEMES } = CONSTANTS
 
 class Header extends Component {
-  render () {  
-      const {theme, setTheme} = this.props
-            const classnames = cx(style.container, {
-            [style.lightTheme]: theme === THEMES.LIGHT,
-            [style.darkTheme]: theme === THEMES.DARK
-          })
+  render () {
+    const { theme, setTheme, user, userLogout } = this.props
+    const classnames = cx(style.container, {
+      [style.lightTheme]: theme === THEMES.LIGHT,
+      [style.darkTheme]: theme === THEMES.DARK
+    })
+    const nextTheme = theme === THEMES.DARK ? THEMES.LIGHT : THEMES.DARK
     return (
-            <UserContext.Consumer>
-              {([user, userLogout]) => {
-                const nextTheme =
-                  theme === THEMES.DARK ? THEMES.LIGHT : THEMES.DARK
-                return (
-                  <div className={classnames}>
-
-                <Switch
-                      checked={theme === THEMES.DARK}
-                      onChange={() => {
-                        setTheme(nextTheme)
-                      }}
-                      inputProps={{ 'aria-label': 'controlled' }}
-                    />
-                    <img src={user.img} style={{ width: '45px' }} />
-                    {user.firstName} {user.lastName}
-                    <button onClick={userLogout}>LogOut</button>
-
-                  </div>
-                )
-              }}
-            </UserContext.Consumer>
-          )
-
+      <div className ={classnames}>
+        <Switch
+          checked={theme === THEMES.DARK}
+          onChange={() => {
+            setTheme(nextTheme)
+          }}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+        <img src={user.img} style={{ width: '45px' }} />
+        {user.firstName} {user.lastName}
+        <button onClick={userLogout}>LogOut</button>
+      </div>
+    )
   }
 }
 
-
 const HeaderWithTheme = withTheme(Header)
-export default HeaderWithTheme
+const HeaderWithUserWithTheme = withUser(HeaderWithTheme)
+export default HeaderWithUserWithTheme

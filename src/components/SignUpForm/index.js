07d35705ec49911898f21fe2621c16be681a1921
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-09-23 13:39:19 +0300

    Formik

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-
-import { SIGN_UP_SCHEMA } from '../../utils/validationSchema'
+import {SIGN_UP_SCHEMA} from '../../utils/validationSchema'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 const initialValues = {
   firstName: '',
@@ -8,58 +8,30 @@ const initialValues = {
   email: '',
   password: ''
 }
-class SignUpForm extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      ...initialValues
-    }
+const SignUpForm = props => {
+  const handleSubmit = (values, formikBag) => {
+    console.log(values, formikBag)
   }
 
-  handlerInput = ({ target: { name, value } }) => {
-    this.setState({
-      [name]: value
-    })
-  }
-  submitHandler = event => {
-    event.preventDefault()
-    const data = this.state
-    console.log(SIGN_UP_SCHEMA.isValidSync(data));
-
-  }
-  render () {
-    const { firstName, lastName, email, password } = this.state
-    return (
-      <form onSubmit={this.submitHandler}>
-        <input
-          type='text'
-          name='firstName'
-          value={firstName}
-          onChange={this.handlerInput}
-        />
-        <input
-          type='text'
-          name='lastName'
-          value={lastName}
-          onChange={this.handlerInput}
-        />
-        <input
-          type='text'
-          name='email'
-          value={email}
-          onChange={this.handlerInput}
-        />
-
-        <input
-          type='text'
-          name='password'
-          value={password}
-          onChange={this.handlerInput}
-        />
-        <button type='submit'>Submit</button>
-      </form>
-    )
-  }
+  return (
+    <>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={SIGN_UP_SCHEMA}>
+        {formikProps => {
+          return (
+            <Form>
+              <Field type='text' name='firstName' />
+              <ErrorMessage name='firstName'/>
+              <Field type='text' name='lastName' />
+              <Field type='text' name='email' />
+              <ErrorMessage name='email' component='p'/>
+              <Field type='text' name='password' />
+              <button type='submit'>Submit</button>
+            </Form>
+          )
+        }}
+      </Formik>
+    </>
+  )
 }
 
 export default SignUpForm

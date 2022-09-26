3ce085ba815755bfca4a3cbb893e3b46d97e9f06
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-09-26 11:11:10 +0300

    useState

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react'
 
 const Home = () => {
+  const [coordinates, setCoordinates] = useState({
+    x: 0,
+    y: 0
+  })
   const [counter, setCounter] = useState(0)
-  const [step, setStep] = useState(1)
-
-  const handler = () => {
-    setCounter(counter + step)
+  const handleMouseMove = ({ clientX, clientY }) => {
+    setCoordinates({
+      x: clientX,
+      y: clientY
+    })
   }
-  const changeHandler = event => {
-    event.preventDefault()
-    const { value } = event.target
-    setStep(Number(value))
+  const clickHandler = () => {
+    setCounter(counter+1)
   }
 
   return (
-    <div>
-      <h1>{counter}</h1>
-      <input type='range' name='step' value={step} onChange={changeHandler} />
-      <button onClick={handler}>Click</button>
+    <div
+      style={{ height: '100vh', border: '4px solid red' }}
+      onMouseMove={handleMouseMove}
+      onClick={clickHandler}
+    >
+      <h1>Mouse tracker</h1>
+      <h2>x: {coordinates.x}</h2>
+      <h2>y: {coordinates.y}</h2>
+      <h2>Counter: {counter}</h2>
     </div>
   )
 }

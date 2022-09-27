a057303dbf2bbdc2d51c790fefa64eee059c9a1d
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-09-27 10:18:47 +0300

    useContext

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Clicker from './components/Clicker'
 import Home from './components/Home'
+import { UserContext } from './contexts'
 import Tracker from './components/Tracker'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-
-  render () {
-    return <Home />
-  }
+const App = () => {
+  const [user, setUser] = useState({
+    id: 1,
+    name: 'John Doe'
+  })
+  return (
+    <div>
+      <UserContext.Provider value={user}>
+        <Home />
+      </UserContext.Provider>
+    </div>
+  )
 }
 
 export default App
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,23 @@
-import React, {useState} from 'react';
-import StopWatch from '../StopWatch';
-import Tracker from '../Tracker';
+import React, { useState, useContext } from 'react'
+import StopWatch from '../StopWatch'
+import Tracker from '../Tracker'
+import { UserContext } from '../../contexts'
 
 const Home = () => {
-    const [isVisible, setVisibility] = useState(true)
-    const handler = ()=>{
-        setVisibility(!isVisible)
-    }
-    return (
-        <div>
-            <button onClick={handler}>click</button>
-            {isVisible && <StopWatch/>}
-        </div>
-    );
+  const [isVisible, setVisibility] = useState(true)
+  const userValue = useContext(UserContext)
+  const handler = () => {
+    setVisibility(!isVisible)
+  }
+  return (
+    <div>
+      <button onClick={handler}>click</button>
+
+      <h1>{userValue.name}</h1>
+
+      {/*             {isVisible && <StopWatch/>} */}
+    </div>
+  )
 }
 
-export default Home;
+export default Home

dce37017f9b87e16065974a80e2b9a203b5e176e
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-09-27 21:39:49 +0300

    Clicker to useClicker

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div>
       <ThemeContext.Provider value={ [theme, setTheme]}>
-        <Loader />
+        <Clicker />
       </ThemeContext.Provider>
     </div>
   )
--- src/components/Clicker/index.js
+++ src/components/Clicker/index.js
@@ -1,29 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import { useClicker } from '../../hooks/useClicker'
 
 const Clicker = () => {
-  const [counter, setCounter] = useState(0)
-const [step, setStep] = useState(1)
 
-  useEffect(() => {
-    const increment = () => {
-        console.log('clicker');
-      setCounter(prevCounter => prevCounter + step)
-    }
-    document.body.addEventListener('click', increment)
-    console.log('навесили');
-    return () => {
-      document.body.removeEventListener('click', increment)
-    console.log('сняли');
-
-    }
-  }, [step])
-const changeHandler = ({target: {value}}) =>{
-    setStep(Number(value))
-}
+  const counter = useClicker()
   return (
     <div>
       <h1>Counter: {counter}</h1>
-      <input type='number' value={step} onChange={changeHandler}/>
+
       <button>Click</button>
     </div>
   )

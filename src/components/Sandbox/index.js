582d46d2100c5fbbe50928542b8a88a100a98f71
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-10-02 16:37:30 +0300

    useDefferedValue

--- src/components/Sandbox/index.js
+++ src/components/Sandbox/index.js
@@ -1,33 +1,30 @@
-import React, {useState, useMemo, useTransition} from 'react';
-import {data} from './data'
+import React, { useState, useMemo, useDeferredValue } from 'react'
+import { data } from './data'
 
 const Sandbox = () => {
-    const [textData, setData] =useState(data)
-    const [inputValue, setInputValue] = useState('')
-    const [isPending, startTransition] = useTransition()
-    const [filterValue, setFilterValue] = useState('')
-    const inputHandler = ({target:{value}}) =>{
-        setInputValue(value)
-        startTransition(()=>{
-            setFilterValue(value)
-        })
-    }
-    const filteredValue = useMemo(()=>{
-        return textData.filter(item => item.text.toLowerCase().includes(inputValue))
-    }, [inputValue])
-
-    const mappedData  = filteredValue.map(obj=>
-        <li key={obj.guid}>{obj.text}</li>
+  const [textData, setData] = useState(data)
+  const [inputValue, setInputValue] = useState('')
+  const defferedValue = useDeferredValue(inputValue)
+  const inputHandler = ({ target: { value } }) => {
+    setInputValue(value)
+  }
+  const filteredValue = useMemo(() => {
+    return textData.filter(item =>
+      item.text.toLowerCase().includes(defferedValue)
     )
+  }, [inputValue])
 
-    return (
-        <div>
-        <input type='text'  value={inputValue} onChange={inputHandler}/>
-<ul>
-    {mappedData}
-</ul>
-        </div>
-    );
+  const mappedData = filteredValue.map(obj => (
+    <li key={obj.guid}>{obj.text}</li>
+  ))
+
+  return (
+    <div>
+      <input type='text' value={inputValue} onChange={inputHandler} />{' '}
+
+      <ul>{mappedData}</ul>
+    </div>
+  )
 }
 
-export default Sandbox;
+export default Sandbox
